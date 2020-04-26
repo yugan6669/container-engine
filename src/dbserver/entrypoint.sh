0a7c808a3a1f94514ae5fe968b096d23cab0e979
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-04-26 17:53:52 +0530

    Create entrypoint.sh

---
+++ src/dbserver/entrypoint.sh
@@ -0,0 +1,12 @@
+#mysql -u root -pRoot_123 -e "create database indigo;"
+
+#mysql -u root -pRoot_123 -e "CREATE USER 'krishna'@'%' IDENTIFIED BY 'Krishna_123';"
+
+#mysql -u root -pRoot_123 -e " GRANT ALL ON *.* TO 'krishna'@'%';"
+
+#mysql -u root -pRoot_123 -e "FLUSH PRIVILEGES;"
+
+#git clone https://github.com/krishnamaram2/WebApp.git
+
+#cd WebApp/binary
+#mysql -u <user_name> -p indigo < indigo.sql
