8edfec53fa2081cdc618795415d39cfbbbe1847d
krishnamaram2 <48556990+krishnamaram2@users.noreply.github.com>
2020-06-29 01:03:44 +0530

    Update run-tomcat.sh

--- src/appserver/run-tomcat.sh
+++ src/appserver/run-tomcat.sh
@@ -1,3 +1,3 @@
-docker cp  server.xml <container_id>:/usr/local/tomcat/webapps/conf/server.xml 
-git clone https://github.com/krishnamaram2/WebApp.git
-docker cp WebApp/binary/Student.war <container_name>:/usr/local/tomcat/webapps/
+#docker cp  server.xml <container_id>:/usr/local/tomcat/webapps/conf/server.xml 
+#git clone https://github.com/krishnamaram2/WebApp.git
+#docker cp WebApp/binary/Student.war <container_name>:/usr/local/tomcat/webapps/
